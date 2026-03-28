41201fadb33c19b41c421dabb3d968b02c6553dd
FelisbinoJose <246760.jose@unibave.net>
2026-03-28 14:31:28 -0300

    feat: adiciona atributo de id e classificação

--- backend/src/model/despesasModel.js
+++ backend/src/model/despesasModel.js
@@ -1,21 +1,27 @@
 const despesas = [
 
     {
+        id: 0,
         nome: "Energia",
         valor: 400.00,
-        dataDeVencimento: "10/03/2026"
+        dataDeVencimento: "10/03/2026",
+        classificacao: "Vencido"
     },
 
     {
+        id: 1,
         nome: "Água",
         valor: 100.00,
-        dataDeVencimento: "11/03/2026"
+        dataDeVencimento: "11/03/2026",
+        classificacao: "Em dia"
     },
 
     {
+        id: 2,
         nome: "Internet",
         valor: 130.00,
-        dataDeVencimento: "09/03/2026"
+        dataDeVencimento: "09/03/2026",
+        classificacao: "Em dia"
     }
 
 ];
